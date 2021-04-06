63bba620e7d3941cda70a930d28ffb053345ed4d
高天宇 <gaotianyu@egret.com>
2021-04-06 16:47:59 +0800

    typescript_koa 03.

--- src/server/index.ts
+++ src/server/index.ts
@@ -4,6 +4,7 @@ import KoaBody from "koa-body";
 import md5 from 'md5';
 import fs from "fs";
 import path from "path";
+import {MysqlHelper} from "./service/mysql.helper"
 
 const app = new Koa();
 const router = new Router();
@@ -35,10 +36,12 @@ router.post('/api/upload', async (ctx) => {
     // 创建可读流
     const reader = fs.createReadStream(element.path);
     // 确认文件存储位置
-    const filePath = getUploadDirName();
+    const rootPath = path.join('./public/file_upload/');
+    checkDirExist(rootPath);
+    const filePath = path.join(rootPath, getDirName());
     checkDirExist(filePath);
     // 新的文件名
-    const fileName = getNewFileName(`${element.name}`);
+    const fileName = getFileName(`${element.name}`);
     // 创建可写流
     const upStream = fs.createWriteStream(`${filePath}/${fileName}`);
     // 可读流通过管道写入可写流
@@ -47,30 +50,82 @@ router.post('/api/upload', async (ctx) => {
   return ctx.body = "上传成功！";
 });
 
+router.post('/api/uploadsingle', async (ctx) => {
+  if (!ctx.request.files || JSON.stringify(ctx.request.files.file) == '{}' || !ctx.request.files.file) {
+    throw new Error('未读取到文件');
+  }
+  const file = ctx.request.files.file;
+  if (Array.isArray(file)) {
+    throw new Error('仅支持单文件上传');
+  }
+  if (file.size < 1000) {
+    throw new Error('文件小于1kb');
+  }
+  const rootPath = path.join('./public/file_upload/');
+  checkDirExist(rootPath);
+  const filePath = path.join(rootPath, getDirName());
+  const fileName = getFileName(file.name ? file.name : '');
+  checkDirExist(filePath);
+  await saveFile(file, `${filePath}/${fileName}`).then(() => {
+    // do something like upload file to cloud.
+    ctx.body = '上传成功';
+  }).catch((err) => {
+    throw new Error(err.message);
+  });
+});
+
+router.get('/api/getdata', async (ctx) => {
+  const mysqlConn = new MysqlHelper('test', 'user1');
+  mysqlConn.connect();
+  await mysqlConn.insert({
+    "firstname": "first",
+    "lastname": "last"
+  })
+  const result = await mysqlConn.select({"id": 1});
+  console.log(result);
+
+  ctx.body = result;
+});
+
 app.use(router.routes());
 
 app.listen(3000);
 
 console.log("Server is running on port 3000");
 
-const getUploadDirName: () => string = (): string => {
+const saveFile = (file: any, path: string) => {
+  return new Promise((resolve, reject) => {
+    const reader = fs.createReadStream(file.path);
+    // console.log({ name: file.name, size: file.size, path: `${filePath}/${fileName}` });
+    const writeStream = fs.createWriteStream(path);
+    reader.pipe(writeStream);
+    writeStream.on('finish', () => {
+      resolve(true);
+    });
+    writeStream.on('error', (err) => {
+      reject(err);
+    });
+  });
+}
+
+const getDirName: () => string = (): string => {
   const date = new Date();
   const monthNumber: number = date.getMonth() + 1;
   const month: string = monthNumber.toString().length > 1 ? monthNumber.toString() : `0${monthNumber}`;
   const dir: string = `${date.getFullYear()}${month}${date.getDate()}`;
-  return path.join('./public/file_upload/', dir);
+  return dir;
+}
+
+const getFileName: (name: string) => string = (name: string): string => {
+  const now = Date.now();
+  const ext = name.split('.');
+  const newname = md5(`${name}|${now}`);
+  const filename = `${newname}.${ext[ext.length - 1]}`;
+  return filename;
 }
 
 const checkDirExist: (dir: string) => void = (dir: string): void => {
   if (!fs.existsSync(dir)) {
     fs.mkdirSync(dir);
   }
-}
-
-const getNewFileName: (name: string) => string = (name: string): string => {
-  const now = Date.now();
-  const ext = name.split('.');
-  const newname = md5(`${name}|${now}`);
-  const filename = `${newname}.${ext[ext.length - 1]}`;
-  return filename;
 }
--- src/server/service/mysql.helper.ts
+++ src/server/service/mysql.helper.ts
@@ -1,16 +1,23 @@
 import mysql from 'mysql';
+import mysqlConf from '../config/mysql.config';
 
-class MysqlHelper {
+const createAsyncAction = async (conn: mysql.Connection, sql: string) => {
+  return new Promise((resolve, reject) => {
+    conn.query(sql, function (err, data) {
+      if (err) {
+        reject(err);
+        return;
+      }
+      resolve(data);
+    })
+  })
+}
+
+export class MysqlHelper {
   private conn;
   private table;
   constructor(database: string, table: string) {
-    this.conn = mysql.createConnection({
-      host: "localhost",
-      user: "root",
-      password: "root",
-      port: 3306,
-      database: database
-    });
+    this.conn = mysql.createConnection(mysqlConf);
     this.table = table;
   }
   // 初始化后需执行该方法建立连接
@@ -32,8 +39,61 @@ class MysqlHelper {
           }
         });
       }, 600000);
-    })
+    });
+  }
+
+  insert(insertObj: any) {
+    var addKey = [];
+    var addVal = [];
+    //解析insertObj对象，拆分为数组方便后续处理
+    for (let i in insertObj) {
+      addKey.push(i);
+      addVal.push(insertObj[i]);
+    }
+    //键值对不匹配，返回错误文档。
+    if (addKey.length != addVal.length) {
+      throw new Error('insert错误，列和值数目不匹配');
+    }
+    //拼接SQL字符串
+    var str = `insert into ${this.table} (`;
+    for (let i = 0; i < addKey.length; i++) {
+      str += addKey[i] + ",";
+    }
+    str = str.substr(0, str.length - 1);
+    str += ") values (";
+    //判断value类型，Mysql区分数字和字符
+    for (let i = 0; i < addVal.length; i++) {
+      if (typeof (addVal[i]) == "string")
+        str += "'" + addVal[i] + "',";
+      else
+        str += +addVal[i] + ",";
+    }
+    str = str.substr(0, str.length - 1);
+    str += ");";
+    //真查询+异步回调
+    return createAsyncAction(this.conn, str)
+  }
+
+  select(queryObj: any) {
+    var str = "";
+    if (queryObj == {}) {
+      str = `select * from ${this.table}`
+    }
+    else {
+      str = `select * from ${this.table} where `;
+      // 区分类型
+      for (var i in queryObj) {
+        if (typeof (queryObj[i]) == "string") {
+          str += `${i}='${queryObj[i]}' and `;
+        }
+        else {
+          str += `${i}=${queryObj[i]} and `;
+        }
+      }
+      // 清除最后的多余字段
+      str = str.substr(0, str.length - 4);
+    }
+    //返回promise
+    return createAsyncAction(this.conn, str)
   }
 }
-
-exports.MysqlHelper = MysqlHelper;
